97e0189dbe0cea2d868844975182d2deaf35830a
gointoorbit <kania.anna@onet.eu>
2024-06-09 10:58:54 +0200

    react-hw-07

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import { AddContactForm } from './AddContactForm/AddContactForm.jsx';
+import { ContactList } from './ContactList/ContactList.jsx';
+import { SearchFilter } from './SearchFilter/SearchFilter.jsx';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from '../redux/operations';
+import { selectIsLoading, selectError } from '../redux/selectors';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1>Phonebook</h1>
+      <AddContactForm />
+      <h2>Contacts</h2>
+      <SearchFilter />
+      {isLoading && !error && <div>Loading in progres...</div>}
+      {error && <div>We cannot download your data. Please try again.</div>}
+      <ContactList />
+    </>
   );
 };
